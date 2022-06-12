d0fce9812f758a4a7d46491440f1800018f74109
ArgjentaGashi <argjenta.gashi1@student.uni-pr.edu>
2022-06-12 22:56:07 +0200

    first jsx files

---
+++ React/First JSX/app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do</h2>
+      <li>
+        <ul>Learn React</ul>
+        <ul>Climb Mt. Everest</ul>
+        <ul>Run a marathon</ul>
+        <ul>Feed the dogs</ul>
+      </li>
+      </div>
+  );
+}
+
+export default App;
